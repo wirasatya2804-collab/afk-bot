33a21fd3224484bb3af994f686ccd2d102d535dd
wirasatya2804-collab <wirasatya2804@gmail.com>
2026-06-12 17:31:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
+import discord
+import os
 
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'Bot {client.user} online!')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content == '!join':
+        if message.author.voice:
+            channel = message.author.voice.channel
+            await channel.connect()
+            await message.channel.send(f'Joined **{channel.name}**!')
+        else:
+            await message.channel.send('Kamu harus masuk voice channel dulu!')
+    
+    if message.content == '!leave':
+        if message.guild.voice_client:
+            await message.guild.voice_client.disconnect()
+            await message.channel.send('Bot keluar dari voice channel!')
+
+client.run(os.environ['TOKEN'])
